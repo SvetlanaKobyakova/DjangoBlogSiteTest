0aecc038d8d7f810663ba5c2b8869757f59ab10c
SvetlanaKobyakova <kobyakasv@mail.ru>
2025-04-08 16:33:26 +0300

    0804

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from blog.views import index, about
+
+app_name = 'blog'
+urlpatterns = [
+    path('about/', about, name='about'),
+    path('', index, name='index'),
+]
---
+++ blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    context = {'title': 'Главная страница'}
+    return render(request, template_name='blog/index.html', context=context)
+
+def about(request):
+    context = {'title': 'О сайте'}
+    return render(request, template_name='blog/about.html', context=context)
